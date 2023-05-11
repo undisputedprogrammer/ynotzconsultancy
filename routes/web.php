8bd19864cd0e1e59518066e32412ac7df44a6b8a
MasterOfNothin <786.ali.nurudheen.3348@gmail.com>
2023-05-11 09:15:38 +0530

    final commit

--- routes/web.php
+++ routes/web.php
@@ -38,9 +38,9 @@ Route::get('/blog/{id}', function(){
     return view('blog');
 });
 
-Route::get('/test', function(){
-    return view('test');
-});
+// Route::get('/test', function(){
+//     return view('test');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
